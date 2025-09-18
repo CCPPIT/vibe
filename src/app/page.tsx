0daca12414d34497fa3be27607b7e287db91f252
CCPPIT <almawisti.cchh@gmail.com>
2025-09-18 14:44:18 -0700

    02:database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
-export default function Home() {
-  return (
-    <Button>Click</Button>
-  )
+import React from 'react'
+  
+  
+  
+  const Page = () => {
+    return (
+      <div>Hello</div>
+    )
   }
+  export default Page
